2d5371fea7eeb9b9e383a1a8e8ac24cc5ee89fa7
blackflypress <76153831+blackflypress@users.noreply.github.com>
2024-06-14 12:19:10 -0700

    Add files via upload

--- gamepad.js
+++ gamepad.js
@@ -1,22 +1,27 @@
 
-// Mobile Controller v.1.0
+// Mobile Controller v.2.0
 // Coded by Loroko 
 // Created: 6/8/2024
 // Last Updated: 6/10/2024
 // blackflypress.com 
 
-const tapThreshold = 150;
-const swipeThreshold = 100
+const tapThreshold = 250;
+const swipeThreshold = 100;
+const moveThreshold = 150;
 
-class Example extends Phaser.Scene{
+class Gamepad extends Phaser.Scene{
 
     constructor(){
-        super('Touchpad');
+        super('Gamepad');
         this.touch = false;
         this.moveActive = false
         this.movePointer = null;
         this.gestureActive = false;
         this.gesturePointer = null;
+        this.moveAngle = 0; // get data
+        this.moveDistance = 0;
+        this.moveForce = 0;
+
       }// end constructor 
 
       preload() {
@@ -30,15 +35,16 @@ class Example extends Phaser.Scene{
         this.add.line(config.width/2, config.height/2, 0, config.height, 0, 0, 0x00ff00);
         this.add.bitmapText(50, 25 , 'p2', `Analog Control`, 45).setOrigin(0,0).setTint(0x00ff00);
         this.add.bitmapText(config.width/2 + 50, 25 , 'p2', `Gesture Control`, 45).setOrigin(0,0).setTint(0x00ff00);
-        this.textAngle = this.add.bitmapText(50, 950 , 'p2', `Angle:`, 45).setOrigin(0,0).setTint(0x00ff00);
+        this.textAngle = this.add.bitmapText(50, 900 , 'p2', `Angle:`, 45).setOrigin(0,0).setTint(0x00ff00);
+        this.textForce = this.add.bitmapText(50, 950 , 'p2', `Force:`, 45).setOrigin(0,0).setTint(0x00ff00);
         this.textDistance = this.add.bitmapText(50, 1000 , 'p2', `Distance:`, 45).setOrigin(0,0).setTint(0x00ff00);
         this.textGesture = this.add.bitmapText(config.width/2 + 50, 1000 , 'p2', `Gesture:`, 45).setOrigin(0,0).setTint(0x00ff00);
         this.touchButton = this.add.image(0, 0, 'touchButton');
         this.touchOrigin = this.add.image(0, 0, 'touchOrigin'); // create touchpad assets
         this.touchCurrent = this.add.image(0, 0, 'touchCurrent');
-        this.touchButton.setScale(6);// scale up
-        this.touchOrigin.setScale(6);
-        this.touchCurrent.setScale(6);
+        this.touchButton.setScale(10);// scale up
+        this.touchOrigin.setScale(10);
+        this.touchCurrent.setScale(10);
         this.touchButton.alpha = 0; // reduce opacity
         this.touchOrigin.alpha = 0; 
         this.touchCurrent.alpha = 0;
@@ -58,6 +64,8 @@ class Example extends Phaser.Scene{
       
         update() {
           this.textAngle.setText('Angle: ' + this.moveAngle); // UI updates
+          this.textForce.setText('Force: ' + this.moveForce); // UI updates
+
           this.textDistance.setText('Distance: ' + this.moveDistance);
          
           if (this.touch == false && this.input.pointer1.active) { // first point of contact
@@ -68,43 +76,71 @@ class Example extends Phaser.Scene{
             if (this.movePointer.active && this.movePointer.downX < config.width/2) { // handle analog control
               this.moveActive = true;
               // console.log('move');
-              this.touchOrigin.alpha = .6; // increase opacity
-              this.touchCurrent.alpha = .6;
+              this.touchOrigin.alpha = .2; // increase opacity
+              this.touchCurrent.alpha = .2;
               this.touchOrigin.setPosition(this.movePointer.downX, this.movePointer.downY); // assign coordinates
               this.touchCurrent.setPosition(this.movePointer.x, this.movePointer.y); 
               this.moveAngle = Math.trunc(this.movePointer.getAngle() * 180/Math.PI); // get data
               this.moveDistance = Math.trunc(this.movePointer.getDistance());
+              this.moveDistance = Phaser.Math.Clamp(this.moveDistance, 0, moveThreshold);
+              this.moveForce = Math.trunc(this.moveDistance / moveThreshold*100);
+
+              if (this.moveDistance == moveThreshold) { // limit distance of current visually
+                Phaser.Math.RotateAroundDistance(this.touchCurrent, this.touchOrigin.x, this.touchOrigin.y, 0, moveThreshold);
+              }// end if (this.distance
+
             }// end if (this.movePointer.active...
       
             if (!this.movePointer.active) {      // handle analog control end
               this.moveActive = false;
+              this.touchOrigin.setPosition(0, 0); // assign coordinates
+              this.touchCurrent.setPosition(0, 0); 
               this.touchOrigin.alpha = 0; // reduce opacity
               this.touchCurrent.alpha = 0;
+              this.moveAngle = 0; // get data
+              this.moveDistance = 0;
+              this.moveForce = 0;
+        
             }// end if (!this.movePointer.active...
       
             if (this.gesturePointer.active && this.gesturePointer.downX > config.width/2) { // handle gesture control
               this.gestureActive = true;
-              this.touchButton.alpha = .6;
+              this.touchButton.alpha = .2;
               this.touchButton.setPosition(this.gesturePointer.x, this.gesturePointer.y); //assign coordinates
               this.gestureDistance = Math.trunc(this.gesturePointer.getDistance());
               this.gestureDistanceX = Math.trunc(this.gesturePointer.getDistanceX());
               this.gestureDistanceY = Math.trunc(this.gesturePointer.getDistanceY());
               this.gestureAngle = Math.trunc(this.gesturePointer.getAngle() * 180/Math.PI);
               this.gestureDuration = Math.trunc(this.gesturePointer.getDuration());
+              this.gestureUp = this.gesturePointer.upTime;
+
+
+              if (this.gestureDuration > tapThreshold) { // HOLD
+                this.textGesture.setText('Gesture: Hold');
+              }// end if (this.gestureDuration...
+              
             }// if (this.gesturePointer.active
       
             if (!this.gesturePointer.active && this.gestureActive == true) { // TAP
               this.gestureActive = false;
               this.touchButton.alpha = 0; //reduce opacity
+
               if (this.gestureDistance < swipeThreshold){ //Check Tap vs Release distance
                 if (this.gestureDuration < tapThreshold) { 
                   this.textGesture.setText('Gesture: Tap');
+                  this.gestureDown = this.gesturePointer.downTime;
+          
+                  if (this.gestureDown - this.gestureUp < 120) {
+                    this.textGesture.setText('Gesture: Double Tap');
+                  }
+
+
                 }// end if (this.gestureDuration...
+
       
-                if (this.gestureDuration > tapThreshold) { // HOLD
-                  this.textGesture.setText('Gesture: Hold');
-                }// end if (this.gestureDuration...
               }// end if (this.gestureDistance...
+
+              if (this.gestureDuration < 225){
       
               if (this.gestureDistance > swipeThreshold) { // SWIPES
                 if (this.gestureDistanceX > this.gestureDistanceY) {
@@ -126,6 +162,7 @@ class Example extends Phaser.Scene{
                 }// end if (this.gestureDistanceX...
               }// end if (this.gestureDistance...
             }// end if (!this.gesturePointer.active...
+          }
           
             if (!this.gesturePointer.active && !this.movePointer.active) { // handle all touch end
               this.touch = false;
@@ -160,7 +197,7 @@ const config = {
         autoCenter: Phaser.Scale.CENTER_BOTH
     },// end scale
     scene: [
-        Example
+        Gamepad
     ]// end scene
     // seed: [ (Date.now() * Math.random()).toString() ]
   }; // end config
